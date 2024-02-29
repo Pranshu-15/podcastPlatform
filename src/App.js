73327e7176043db0ddfe46d97c0cc29e9569281d
Pranshu-15 <pranshu923@gmail.com>
2024-02-29 05:51:09 +0530

    video11finished

--- src/App.js
+++ src/App.js
@@ -1,30 +1,127 @@
-import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+// import {BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+// import './App.css';
+// import SignUpPage from './Pages/SignUpPage';
+// import ProfilePage from './Pages/ProfilePage';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { useEffect } from 'react';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import { auth, db } from './firebase';
+// import { doc, onSnapshot } from 'firebase/firestore';
+// import { useDispatch } from 'react-redux';
+// import { setUser } from './slices/userSlice';
+
+
+// const App = () => {
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     const unsubscribeAuth = onAuthStateChanged (auth,(user) => {
+//       if(user){
+//         const unsubscribeSnapshot = onSnapshot(
+//           doc(db, "users", user.uid),
+//           (userDoc) => {
+//             if(userDoc.exists()) {
+//               const userData = userDoc.data();
+//               dispatch(
+//                 setUser({
+//                   name:userData.name,
+//                   email:userData.email,
+//                   uid:user.uid,
+//                 })
+//               );
+//             }
+//           },
+//           (error) =>{
+//             console.log("Error fetching user data:" , error);
+//           }
+//         );
+//         return () => {
+//           unsubscribeSnapshot();
+//         };
+//       };
+//     });
+    
+//     return unsubscribeAuth();
+//   },[])
+//   return (
+//     <>
+//     <ToastContainer />
+//        <Router>
+//           <Routes>
+//             <Route path='/' element = {<SignUpPage />}/>
+//             <Route path='/profile' element = {<ProfilePage/>}/>
+//           </ Routes>
+//         </Router >
+//     </>
+//   )
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUpPage from './Pages/SignUpPage';
 import ProfilePage from './Pages/ProfilePage';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
-
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from './firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { useDispatch } from 'react-redux';
+import { setUser } from './slices/userSlice';
+import PrivateRoutes from './Component/commonComponents/PrivateRoutes';
 
 const App = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const authUnsubscribe = () => {};
-    
-    return authUnsubscribe();
-  },[])
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const unsubscribeSnapshot = onSnapshot(
+          doc(db, "users", user.uid),
+          (userDoc) => {
+            if (userDoc.exists()) {
+              const userData = userDoc.data();
+              dispatch(
+                setUser({
+                  name: userData.name,
+                  email: userData.email,
+                  uid: user.uid,
+                })
+              );
+            }
+          },
+          (error) => {
+            console.log("Error fetching user data:", error);
+          }
+        );
+        return () => {
+          unsubscribeSnapshot();
+        };
+      }
+    });
+
+    return () => {
+      unsubscribeAuth();
+    };
+  }, [dispatch]);
+
   return (
     <>
-    <ToastContainer />
-       <Router>
-          <Routes>
-            <Route path='/' element = {<SignUpPage />}/>
-            <Route path='/profile' element = {<ProfilePage/>}/>
-          </ Routes>
-        </Router >
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path='/' element={<SignUpPage />} />
+          <Route element = {<PrivateRoutes/>}>
+          <Route path='/profile' element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
 export default App;
+
 
--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../Component/commonComponents/Header";
+import Button from "../Component/commonComponents/Button";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+import { toast } from "react-toastify";
 
 const ProfilePage = () => {
     const user = useSelector((state)=> state.user.user);
+    if(!user){
+        return <p>Loading......</p>
+    }
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            toast.success("User Logged Out!!")
+            
+        }).catch((error) => {
+            toast.error(error.message)
+          });
+    }
+ 
     return(
         <>
         <Header/>
             <div><h1>Hello {user.name}</h1></div>
+            <Button text={"LogOut"} onClick={handleLogout}/>
         </>
     )
 }
 export default ProfilePage;
+
